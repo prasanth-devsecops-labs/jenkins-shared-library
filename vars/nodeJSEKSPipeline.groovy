6563947d1a68f1f5e8a3eb88f24324b4907e3958
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-05-01 17:23:24 +0530

    jenkins

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -43,10 +43,14 @@ def call(Map configMap){
             }
             stage('Unit tests') {
                 steps {
-                    script{
-                        sh """
-                            npm test
-                        """
+                    script {
+                        def testResult = sh(script: 'npm test', returnStatus: true)
+                        if (testResult != 0) {
+                            utils.updateCommitStatus('failure', 'Unit tests failed', 'unit-tests')
+                            error "Unit tests failed."
+                        } else {
+                            utils.updateCommitStatus('success', 'Unit tests passed', 'unit-tests')
+                        }
                     }
                 }
             }
